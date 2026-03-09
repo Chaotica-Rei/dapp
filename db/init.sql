eee6379a90ee7bf251cdc397aef0a048dfadf45e
Chaotica-Rei <chaotica.rei@gmail.com>
2026-03-09 12:42:16 +0700

    added test user creation instructions

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,22 @@
--- Создаем БД test и таблицу users 
-
+-- Удаляем БД если существует
 DROP DATABASE IF EXISTS test;
 
-CREATE DATABASE test;
+-- Удаляем пользователя если существует
+DROP ROLE IF EXISTS testuser;
 
+-- Создаем пользователя
+CREATE USER testuser WITH PASSWORD 'testpass';
+
+-- Создаем БД test
+CREATE DATABASE test OWNER testuser;
+
+-- Подключаемся к базе test
 \c test
 
+-- Удаляем таблицу если есть
 DROP TABLE IF EXISTS users;
 
+-- Создаем таблицу
 CREATE TABLE users (
  id SERIAL PRIMARY KEY,
  name VARCHAR(50),
@@ -15,6 +24,9 @@ CREATE TABLE users (
  email VARCHAR(100)
 );
 
--- Импортируем данные из CSV файла в таблицу users
+-- Даем права пользователю
+GRANT ALL PRIVILEGES ON TABLE users TO testuser;
+GRANT ALL PRIVILEGES ON SEQUENCE users_id_seq TO testuser;
 
+-- Импортируем CSV
 \copy users(name, age, email) FROM :'csv_path' DELIMITER ',' CSV HEADER;
