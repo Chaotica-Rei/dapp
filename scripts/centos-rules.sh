02383a7a0a4e858430c6c0dc19388224b0d48a31
Chaotica-Rei <chaotica.rei@gmail.com>
2026-03-11 12:54:50 +0700

    fixed network rules

--- scripts/centos-rules.sh
+++ scripts/centos-rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Applying iptables rules for PROXY (CentOS)..."
+echo "Applying iptables rules for PROXY (CentOS Stream 10)..."
 
 # Очистка
 iptables -F
@@ -19,23 +19,13 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# -------------------------
-# Proxy принимает запросы от любого источника
-# -------------------------
-
-# Proxy API (5000)
+# Proxy принимает запросы  на порт 5000 от любого источника
 iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 
-# -------------------------
-# Proxy имеет доступ к Backend
-# -------------------------
-
+# Proxy имеет доступ к Backend (указывать фактический IP-адрес Backend)
 iptables -A OUTPUT -p tcp -d 192.168.0.177 --dport 8080 -j ACCEPT
 
-# -------------------------
 # Proxy имеет доступ к Redis (локально)
-# -------------------------
-
 iptables -A OUTPUT -p tcp --dport 6379 -j ACCEPT
 iptables -A INPUT -p tcp --sport 6379 -j ACCEPT
 
--- scripts/ubuntu-rules.sh
+++ scripts/ubuntu-rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Applying iptables rules for BACKEND (Ubuntu)..."
+echo "Applying iptables rules for BACKEND (Ubuntu 24.04.3 LTS)..."
 
 # Очистка
 iptables -F
@@ -19,22 +19,13 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# -------------------------
-# Backend принимает запросы ТОЛЬКО от Proxy
-# -------------------------
-
+# Backend принимает запросы только от прокси (указать фактический IP-адрес прокси)
 iptables -A INPUT -p tcp -s 192.168.0.117 --dport 8080 -j ACCEPT
 
-# -------------------------
 # Backend имеет доступ только к PostgreSQL
-# -------------------------
-
 iptables -A OUTPUT -p tcp --dport 5432 -j ACCEPT
 
-# -------------------------
 # PostgreSQL принимает только от Backend (локально)
-# -------------------------
-
 iptables -A INPUT -p tcp -s 127.0.0.1 --dport 5432 -j ACCEPT
 
 echo "Backend rules applied"
